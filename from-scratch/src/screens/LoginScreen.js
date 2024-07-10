946795335ea86dcbc333516b8c2205c83d4e487d
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-07-10 19:02:12 +0500

    create the login screen which uses our Auth Context to sign a user in

---
+++ from-scratch/src/screens/LoginScreen.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useAuth } from '../context/AuthContext';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login } = useAuth();
+  const navigation = useNavigation();
+
+  const handleNavigateToRegister = () => {
+    navigation.navigate('Register');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={() => login(email, password)} />
+      <TouchableOpacity title="Register" onPress={handleNavigateToRegister} style={styles.registerButton}>
+        <Text>Register</Text>
+      </TouchableOpacity>
+      {/* <Button title='register' onPress={handleNavigateToRegister}/> */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+  registerButton: {
+    marginTop: 16,
+    backgroundColor: 'gray',
+  }
+});
+
+export default LoginScreen;
