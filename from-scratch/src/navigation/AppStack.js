75445619e047db7b46c87c1d569739901a16f6bb
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-07-16 20:39:13 +0500

    fix: hide the header of Drawer when inside the ChatNavigator

--- from-scratch/src/navigation/AppStack.js
+++ from-scratch/src/navigation/AppStack.js
@@ -30,7 +30,7 @@ function Sidebar(role) {
     return (
       <>
         <Drawer.Screen name="Home" component={HomeStack} />
-        <Drawer.Screen name="Chat" component={ChatNavigator} />
+        <Drawer.Screen name="Chat" component={ChatNavigator} options={{ headerShown: false, }} />
         <Drawer.Screen name="All Listings" component={ListingStackScreen} initialParams={{ limit: 20 }} />
         <Drawer.Screen name="My Listings" component={ListingStackScreen} initialParams={{ fromUser: true, limit: 10 }} />
         <Drawer.Screen name="Create Listing" component={ListingForm} />
@@ -43,7 +43,7 @@ function Sidebar(role) {
     return (
       <>
         <Drawer.Screen name="Home" component={HomeStack} options={{ headerTitle: 'Home' }} />
-        <Drawer.Screen name="Chat" component={ChatNavigator} />
+        <Drawer.Screen name="Chat" component={ChatNavigator} options={{ headerShown: false, }} />
         <Drawer.Screen name="All Listings" component={Listing} initialParams={{ limit: 20 }} />
         <Drawer.Screen name="My Listings" component={Listing} initialParams={{ fromUser: true, limit: 10 }} />
         <Drawer.Screen name="Create Listing" component={ListingForm} />
@@ -56,7 +56,7 @@ function Sidebar(role) {
     <>
       <Drawer.Screen name="Home" component={HomeStack} />
       <Drawer.Screen name="Listings" component={ProfileScreen} />
-      <Drawer.Screen name="Chat" component={ChatNavigator} />
+      <Drawer.Screen name="Chat" component={ChatNavigator} options={{ headerShown: false, }} />
     </>
   );
 }
