6cbdd5adacbe20527f9995bc67d33bebf551c214
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-07-19 14:24:55 +0500

    feature: setup Messages screen along with textInput at the bottom, send functionality currently not working

--- from-scratch/src/screens/ChatMessages.js
+++ from-scratch/src/screens/ChatMessages.js
@@ -1,17 +1,19 @@
 import { useState, useEffect } from 'react';
-import { Text, ScrollView, View, StyleSheet, Button } from 'react-native';
+import { Text, TextInput, TouchableOpacity, ScrollView, View, StyleSheet, Button } from 'react-native';
 import axios from 'axios';
 import { socket } from '../../App';
+import localTime from '../utils/localDateTime.js';
 
 function ChatMessages({ route }) {
   const { chatId, userId } = route.params;
   const [messages, setMessages] = useState([]);
+  const [userMessage, setUserMessage] = useState('');
 
   useEffect(() => {
     async function getMessages(lastMessageDate = null) {
       try {
         const response = await axios.get(`/api/c/chats/${chatId}/messages`, {
-          params: { 
+          params: {
             userId,
             lastMessageDate,
             limit: 20
@@ -20,21 +22,21 @@ function ChatMessages({ route }) {
         const newMessages = response.data;
         console.log('messages received:', newMessages);
         setMessages((prevMessages) => [...newMessages, ...prevMessages]);
-      } catch(error) {
+      } catch (error) {
         console.log('Error fetching messages:', error);
       }
     }
 
     getMessages();
 
-    // for socket connection
+    // Socket connection
     socket.on('connect', () => {
       console.log('socket connected');
     });
 
     socket.on('newMessage', (data) => {
       console.log('received message:', data);
-      setMessages((prevMessages) => [...prevMessages, data]);
+      setMessages((prevMessages) => [data, ...prevMessages]);
     });
 
     return () => {
@@ -50,24 +52,45 @@ function ChatMessages({ route }) {
 
   function pushTempMessage() {
     const tempMessage = {
-      message: 'temp message',
-      userId: userId, // Assuming messages have a userId field
-      timestamp: new Date().toISOString() // Add a timestamp or other fields as necessary
+      message: userMessage,
+      userId: userId,
+      timestamp: new Date().toISOString()
     };
-    setMessages((prevMessages) => [...prevMessages, tempMessage]);
-    // Simulate receiving the message via socket
+    setMessages((prevMessages) => [tempMessage, ...prevMessages]);
+    setUserMessage('');
     socket.emit('tempMessage', tempMessage);
   }
 
+  const sendMessage = (message) => {
+    const receiverId = currentChat.userId === userId ? currentChat.agentId : currentChat.userId;
+    const newMessage = {
+      chatId: currentChat._id,
+      senderId: userId,
+      receiverId,
+      message,
+    };
+    socket.emit('sendMessage', newMessage);
+    setMessages(prev => [newMessage, ...prev]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Messages of each Chat {chatId}</Text>
 
-      <ScrollView style={styles.scrollView}>
+      <ScrollView 
+        style={styles.scrollView}
+        showsVerticalScrollIndicator={false}
+      >
         {messages.length > 0 ? (
           messages.map((message, index) => (
-            <View key={index} style={styles.messageContainer}>
+            <View
+              key={index}
+              style={[
+                styles.messageContainer,
+                message.senderId === userId ? styles.messageSent : styles.messageReceived
+              ]}
+            >
               <Text style={styles.messageText}>{message.message}</Text>
+              <Text style={styles.messageTimestamp}>{localTime(message.createdAt)}</Text>
             </View>
           ))
         ) : (
@@ -75,7 +98,17 @@ function ChatMessages({ route }) {
         )}
       </ScrollView>
 
-      <Button onPress={pushTempMessage} title="Add a temp message" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={userMessage}
+          onChangeText={setUserMessage}
+          placeholder="Type a message..."
+        />
+        <TouchableOpacity onPress={pushTempMessage} style={styles.sendButton}>
+          <Text>Send</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -83,22 +116,56 @@ function ChatMessages({ route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
+    paddingHorizontal: 16,
     backgroundColor: '#f5f5f5',
   },
   scrollView: {
     flex: 1,
-    borderWidth: 1,
-    borderColor: 'red',
+    transform: [{ rotate: '180deg'}],
+    paddingBottom: 10,
+    paddingTop: 10,
   },
   messageContainer: {
     padding: 10,
-    borderWidth: 1,
-    borderColor: 'blue',
-    marginVertical: 5,
+    borderRadius: 10,
+    marginBottom: 5,
+    maxWidth: '70%',
+    alignSelf: 'flex-start',
+    transform: [{ rotate: '180deg'}],
+  },
+  messageSent: {
+    backgroundColor: '#D0FDF5',
+    alignSelf: 'flex-start',
+  },
+  messageReceived: {
+    backgroundColor: '#f0f0f0',
+    alignSelf: 'flex-end',
   },
   messageText: {
-    color: '#333',
+    color: '#0b141d',
+  },
+  messageTimestamp: {
+    fontSize: 8,
+    marginTop: 5,
+    alignSelf: 'flex-end',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    marginBottom: 25,
+    padding: 10,
+    borderTopWidth: 1,
+    borderTopColor: '#DDDDDD',
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+  },
+  sendButton: {
+    justifyContent: 'center',
+    padding: 10,
   },
 });
 
