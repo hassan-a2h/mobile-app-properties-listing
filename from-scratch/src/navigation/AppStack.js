4d92c6294f3738719717331d69f7ab3515cf4566
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-07-23 11:43:11 +0500

    feature: realtime message notifications
    details:
    * in the appStack component import socket and fetch the unreadMessages then pass it to the custom header

--- from-scratch/src/navigation/AppStack.js
+++ from-scratch/src/navigation/AppStack.js
@@ -1,40 +1,60 @@
-import React, { useEffect, useState } from 'react';
+// AppStack.js
+
+import React, { useEffect, useState, useCallback } from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createStackNavigator } from '@react-navigation/stack';
 import CustomDrawerContent from '../navigation/CustomDrawer/CustomDrawer';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import CustomDrawerTopbar from '../components/CustomDrawerTopbar';
 import Sidebar from '../utils/sidebarOptions';
+import fetchUnreadMessages from '../utils/fetchUnreadMessages';
+import { useAuth } from '../context/AuthContext';
+import { initSocket, getSocket } from '../sockets/socketService';
 
 const Drawer = createDrawerNavigator();
-const Stack = createStackNavigator();
-
-
 
 const AppStack = () => {
-  const [ userRole, setUserRole ] = useState(null);
+  const { user } = useAuth();
+  const [unreadMessages, setUnreadMessages] = useState(0);
+
+  const updateUnreadMessages = useCallback(async () => {
+    console.log('!!!! Updating unread messages...');
+    try {
+      const unread = await fetchUnreadMessages(user?.id);
+      setUnreadMessages(unread);
+      console.log('unread messages updated...', unread);
+    } catch (error) {
+      console.error('Error updating unread messages:', error);
+    }
+  }, [user]);
 
   useEffect(() => {
-    async function getRole() {
-      const role = await AsyncStorage.getItem('role');
-      setUserRole(prevRole => role);
+    if (user) {
+      initSocket(user.id, updateUnreadMessages);
     }
-    
-    getRole();
-  }, []);
 
-  console.log('role:', userRole);
+    const socket = getSocket();
+
+    // Initial fetch of unread messages
+    updateUnreadMessages();
+
+    return () => {
+      if (socket) {
+        socket.off('receiveMessage');
+        socket.off('unreadCountUpdated');
+      }
+    };
+  }, [user, updateUnreadMessages]);
+
+  console.log('Current unread messages:', unreadMessages);
 
   return (
     <Drawer.Navigator
-      drawerContent={ (props) => <CustomDrawerContent {...props} />}
-      screenOptions={ ({ route }) => ({ 
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={({ route }) => ({ 
         headerShown: true,
-        header: () => <CustomDrawerTopbar title={route.name} value={2} />
-       })}
+        header: () => <CustomDrawerTopbar title={route.name} value={unreadMessages?.unreadCount} />
+      })}
     >
-      
-      {Sidebar(userRole)}
+      {Sidebar(user.role)}
     </Drawer.Navigator>
   );
 };
