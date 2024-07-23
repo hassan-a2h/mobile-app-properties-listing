a998262680b094bdc41227beeb6813eefbc44bfb
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-07-23 11:41:33 +0500

    move the socket initiation logic to a separate component

--- from-scratch/App.js
+++ from-scratch/App.js
@@ -1,18 +1,30 @@
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { AuthProvider } from './src/context/AuthContext';
+import { AuthProvider, useAuth } from './src/context/AuthContext';
 import MainNavigator from './src/navigation/MainNavigator';
 import Toast from 'react-native-toast-message';
-import io from 'socket.io-client';
-import getBackendAddress from './src/utils/getBackendAddress';
-const address = getBackendAddress();
+import { initSocket, disconnectSocket } from './src/sockets/socketService';
 
-export const socket = io(address, { transports: ['websocket'] });
+const AppContent = () => {
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (user) {
+      initSocket(user.id);
+    }
+    return () => {
+      disconnectSocket();
+    };
+  }, [user]);
+
+  return <MainNavigator />;
+};
 
 export default function App() {
   return (
     <NavigationContainer>
       <AuthProvider>
-        <MainNavigator />
+        <AppContent />
       </AuthProvider>
       <Toast position='bottom' />
     </NavigationContainer>
