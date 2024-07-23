970d7f0ae75a08e8fe273d430d6f6be911299155
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-07-23 12:35:33 +0500

    update sidebar to reset when user logs out. allow user to view listings

--- from-scratch/src/utils/sidebarOptions.js
+++ from-scratch/src/utils/sidebarOptions.js
@@ -1,10 +1,10 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from '../screens/HomeScreen';
 import ChatNavigator from '../navigation/CustomNavigators/ChatNavigator';
-import ProfileScreen from '../screens/ProfileScreen';
 import ListingStackScreen from '../navigation/CustomNavigators/ListingNavigator';
 import Listing from '../components/Listing/Listing';
 import ListingForm from '../screens/ListingForm';
+import Register from '../screens/RegisterScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -17,7 +17,7 @@ function sidebarOptions(role) {
         <Drawer.Screen name="All Listings" component={ListingStackScreen} initialParams={{ limit: 20 }} />
         <Drawer.Screen name="My Listings" component={ListingStackScreen} initialParams={{ fromUser: true, limit: 10 }} />
         <Drawer.Screen name="Create Listing" component={ListingForm} />
-        <Drawer.Screen name="Add User" component={ProfileScreen} />
+        <Drawer.Screen name="Add User" component={Register} />
       </>
     );
   }
@@ -30,7 +30,7 @@ function sidebarOptions(role) {
         <Drawer.Screen name="All Listings" component={Listing} initialParams={{ limit: 20 }} />
         <Drawer.Screen name="My Listings" component={Listing} initialParams={{ fromUser: true, limit: 10 }} />
         <Drawer.Screen name="Create Listing" component={ListingForm} />
-        <Drawer.Screen name="Socials" component={ProfileScreen} />
+        <Drawer.Screen name="Socials" component={ListingForm} />
       </>
     );
   }
@@ -38,7 +38,7 @@ function sidebarOptions(role) {
   return (
     <>
       <Drawer.Screen name="Home" component={HomeScreen} />
-      <Drawer.Screen name="Listings" component={ProfileScreen} />
+      <Drawer.Screen name="Listings" component={Listing} initialParams={{ limit: 20 }} />
       <Drawer.Screen name="Chat" component={ChatNavigator} options={{ headerShown: false }} />
     </>
   );
