11f392fd826625c1a87301bd7c69f9dda90b1954
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-07-23 14:01:05 +0500

    update the styling of the custom sidebar drawer

--- from-scratch/src/navigation/CustomDrawer/CustomDrawer.js
+++ from-scratch/src/navigation/CustomDrawer/CustomDrawer.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 import { useAuth } from '../../context/AuthContext';
 
-const CustomDrawer = (props) => {
+const CustomDrawer = ({ unreadMessagesCount, ...props }) => {
   const { logout } = useAuth();
 
   return (
     <DrawerContentScrollView {...props}>
       <View style={styles.drawerContent}>
-        <DrawerItemList {...props} />
-        <DrawerItem style={styles.logout} label="Logout" onPress={logout} />
+        {/* Custom DrawerItemList to add special design for "Chat" */}
+        {props.state.routes.map((route, index) => {
+          const focused = index === props.state.index;
+          if (route.name === 'Chat') {
+            return (
+              <View key={route.key} style={styles.chatItemContainer}>
+                <DrawerItem
+                  label={`Chats ${ unreadMessagesCount > 0 && (unreadMessagesCount > 9 ? '(9+ new)' : '(' + unreadMessagesCount + ' new)') }`}
+                  onPress={() => props.navigation.navigate(route.name)}
+                  labelStyle={[styles.itemText, focused && styles.itemTextFocused]}
+                  style={[styles.chatItem, focused && styles.chatSelected]}
+                />
+              </View>
+            );
+          } else {
+            return (
+              <DrawerItem
+                key={route.key}
+                label={route.name}
+                onPress={() => props.navigation.navigate(route.name)}
+                labelStyle={[styles.itemText, focused && styles.itemTextFocused]}
+                style={[styles.item, focused && styles.itemFocused]}
+              />
+            );
+          }
+        })}
+        <DrawerItem
+          label="Logout"
+          onPress={logout}
+          labelStyle={styles.itemText}
+          style={styles.logout}
+        />
       </View>
     </DrawerContentScrollView>
   );
@@ -20,10 +50,56 @@ const styles = StyleSheet.create({
   drawerContent: {
     flex: 1,
   },
+  chatItemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+  },
+  value: {
+    marginLeft: 10,
+    fontSize: 16,
+    color: '#ffffff',
+    // borderWidth: 1,
+    paddingRight: 8,
+    paddingLeft: 8,
+    paddingTop: 4,
+    paddingBottom: 4,
+    borderRadius: 12,
+    // borderColor: 'gray',
+    backgroundColor: '#FFFFFF',
+  },
+  messageNotification: {
+    color: '#ffffff',
+  },
+  item: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  chatItem: {
+    flex: 1,
+  },
+  itemFocused: {
+    backgroundColor: '#00BE8E',
+  },
+  chatSelected: {
+    backgroundColor: '#00BE8E',
+  },
+  itemText: {
+    color: 'black',
+    textAlign: 'center',
+  },
+  itemTextFocused: {
+    color: 'white',
+  },
+  additionalData: {
+    marginLeft: 10,
+    fontSize: 14,
+    color: 'gray',
+  },
   logout: {
     backgroundColor: '#FFCCCB',
-    color: 'red',
-  }
+  },
 });
 
 export default CustomDrawer;
