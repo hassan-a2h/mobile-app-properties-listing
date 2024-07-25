740cbb19b3b96f809f90460f88a3d638e9a91304
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-07-25 16:39:23 +0500

    suppress warning messages

--- from-scratch/App.js
+++ from-scratch/App.js
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider, useAuth } from './src/context/AuthContext';
 import MainNavigator from './src/navigation/MainNavigator';
+import { LogBox } from 'react-native';
 import Toast from 'react-native-toast-message';
 import { initSocket, disconnectSocket } from './src/sockets/socketService';
 import ErrorBoundary from './src/screens/ErrorBoundary';
 
 const AppContent = () => {
   const { user } = useAuth();
+  LogBox.ignoreAllLogs();
 
   useEffect(() => {
     if (user) {
