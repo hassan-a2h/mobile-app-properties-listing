4b99fe2afe11c408f5cecd7f6f5485dfa197237a
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-07-25 11:48:11 +0500

    update: sidebar to have stylized new message notification

--- from-scratch/src/navigation/CustomDrawer/CustomDrawer.js
+++ from-scratch/src/navigation/CustomDrawer/CustomDrawer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
+import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
 import { useAuth } from '../../context/AuthContext';
 
 const CustomDrawer = ({ unreadMessagesCount, ...props }) => {
@@ -10,13 +10,15 @@ const CustomDrawer = ({ unreadMessagesCount, ...props }) => {
     <DrawerContentScrollView {...props}>
       <View style={styles.drawerContent}>
         {/* Custom DrawerItemList to add special design for "Chat" */}
-        {props.state.routes.map((route, index) => {
+        { props.state.routes.map((route, index) => {
+
           const focused = index === props.state.index;
-          if (route.name === 'Chat') {
+
+          if (route.name === 'Chats') {
             return (
               <View key={route.key} style={styles.chatItemContainer}>
                 <DrawerItem
-                  label={`Chats ${ unreadMessagesCount > 0 && (unreadMessagesCount > 9 ? '(9+ new)' : '(' + unreadMessagesCount + ' new)') }`}
+                  label={ () => ChatDrawerItem(unreadMessagesCount, focused) }
                   onPress={() => props.navigation.navigate(route.name)}
                   labelStyle={[styles.itemText, focused && styles.itemTextFocused]}
                   style={[styles.chatItem, focused && styles.chatSelected]}
@@ -46,28 +48,32 @@ const CustomDrawer = ({ unreadMessagesCount, ...props }) => {
   );
 };
 
+const ChatDrawerItem = (unreadMessagesCount, focused) => {
+  return (
+    <View style={styles.chatDrawerItem}>
+      <Text style={[styles.itemText, focused && styles.itemTextFocused]}>{'Chats'}</Text>
+      { unreadMessagesCount > 0 && 
+        <View style={styles.value}>
+          <Text style={styles.messageNotification}>{unreadMessagesCount > 9 ? '9+' : unreadMessagesCount}</Text>
+        </View> 
+      }
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   drawerContent: {
     flex: 1,
+  },
+  chatDrawerItem: {
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center',
   },
   chatItemContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    paddingVertical: 10,
-  },
-  value: {
-    marginLeft: 10,
-    fontSize: 16,
-    color: '#ffffff',
-    // borderWidth: 1,
-    paddingRight: 8,
-    paddingLeft: 8,
-    paddingTop: 4,
-    paddingBottom: 4,
-    borderRadius: 12,
-    // borderColor: 'gray',
-    backgroundColor: '#FFFFFF',
   },
   messageNotification: {
     color: '#ffffff',
@@ -79,23 +85,35 @@ const styles = StyleSheet.create({
   chatItem: {
     flex: 1,
   },
+  itemText: {
+    color: 'red',
+    fontWeight: 'normal',
+    color: '#4F4F4F',
+  },
   itemFocused: {
     backgroundColor: '#00BE8E',
   },
   chatSelected: {
     backgroundColor: '#00BE8E',
   },
-  itemText: {
-    color: 'black',
-    textAlign: 'center',
+  value: {
+    marginLeft: 8,
+    fontSize: 1,
+    color: '#ffffff',
+    // borderWidth: 1,
+    paddingRight: 8,
+    paddingLeft: 8,
+    paddingTop: 4,
+    paddingBottom: 4,
+    borderRadius: 12,
+    // borderColor: 'gray',
+    backgroundColor: '#6F6F6F',
+  },
+  messageNotification: {
+    color: '#ffffff',
   },
   itemTextFocused: {
     color: 'white',
-  },
-  additionalData: {
-    marginLeft: 10,
-    fontSize: 14,
-    color: 'gray',
   },
   logout: {
     backgroundColor: '#FFCCCB',
