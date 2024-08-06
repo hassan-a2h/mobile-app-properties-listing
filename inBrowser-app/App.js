eee50709b0af7066487b125097fd35e342ce7b1e
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-08-06 17:39:12 +0500

    refactor: inBrowser WebView component

--- inBrowser-app/App.js
+++ inBrowser-app/App.js
@@ -1,23 +1,23 @@
-import { WebView } from 'react-native-webview';
-import { Platform } from 'react-native';
+import { WebView } from "react-native-webview";
+import { Platform } from "react-native";
 
 export default function App() {
-  let webAppUrl = 'http://10.0.2.2:3000';
+  let webAppUrl = "http://10.0.2.2:3000";
 
-  if (Platform.OS === 'ios') {
-    webAppUrl = 'http://localhost:3000';
+  if (Platform.OS === "ios") {
+    webAppUrl = "http://localhost:3000";
   }
 
   return (
     <>
-    <WebView
-      originWhitelist={['*']}
-      source={{ uri: webAppUrl }}
-      style={{ flex: 1, marginTop: 10 }}
-      javaScriptEnabled={true}
-      domStorageEnabled={true}
-      mixedContentMode="always"
-    />
+      <WebView
+        originWhitelist={["*"]}
+        source={{ uri: webAppUrl }}
+        style={{ flex: 1, marginTop: 10 }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        mixedContentMode="always"
+      />
     </>
   );
-};
+}
